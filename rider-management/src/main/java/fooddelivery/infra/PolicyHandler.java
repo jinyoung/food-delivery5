b072206b21617caeca1c8f6ea2b88b23c0c8ea23
Jinyoung Jang <jyjang@uengine.org>
2023-12-27 01:11:39 +0900

    commit

--- rider-management/src/main/java/fooddelivery/infra/PolicyHandler.java
+++ rider-management/src/main/java/fooddelivery/infra/PolicyHandler.java
@@ -1,37 +1,38 @@
-{
 package fooddelivery.infra;
 
-import javax.naming.NameParser;
-
-import javax.naming.NameParser;
-import javax.transaction.Transactional;
-
-import fooddelivery.config.kafka.KafkaProcessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import fooddelivery.config.kafka.KafkaProcessor;
+import fooddelivery.domain.*;
+import javax.naming.NameParser;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
-import fooddelivery.domain.*;
 
-//<<< Clean Arch / Inbound Adaptor
 @Service
 @Transactional
-public class PolicyHandler{
-    @Autowired RiderRepository riderRepository;
-    
+public class PolicyHandler {
+
+    @Autowired
+    RiderRepository riderRepository;
+
     @StreamListener(KafkaProcessor.INPUT)
-    public void whatever(@Payload String eventString){}
+    public void whatever(@Payload String eventString) {}
 
-    @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='OrderPlaced'")
-    public void wheneverOrderPlaced_OrderPlacedPolicy(@Payload OrderPlaced orderPlaced){
-
+    @StreamListener(
+        value = KafkaProcessor.INPUT,
+        condition = "headers['type']=='OrderPlaced'"
+    )
+    public void wheneverOrderPlaced_OrderPlacedPolicy(
+        @Payload OrderPlaced orderPlaced
+    ) {
         OrderPlaced event = orderPlaced;
-        System.out.println("\n\n##### listener OrderPlacedPolicy : " + orderPlaced + "\n\n");
+        System.out.println(
+            "\n\n##### listener OrderPlacedPolicy : " + orderPlaced + "\n\n"
+        );
 
-
-        // Sample Logic //
         Rider rider = new Rider();
         rider.setRiderId("1");
         rider.setRiderName("Rider1");
@@ -43,9 +44,4 @@ public class PolicyHandler{
         orderAssigned.setOrderId(event.getOrderId());
         orderAssigned.publishAfterCommit();
     }
-
-}
-
-//>>> Clean Arch / Inbound Adaptor
-
 }
